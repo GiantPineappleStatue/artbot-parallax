d8b6b3fff213310d4b2efa48cceb807418397b36
Jerry Wu <jerrywu.hr@gmail.com>
2023-02-13 10:03:18 -0800

    updates

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,71 +1,65 @@
-import './header.css'
+import './header.css';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Sidebar from '../sidebar/sidebar';
-import logo from '../../assets/ARTBOT.png'
+import logo from '../../assets/ARTBOT.png';
 import { useNavigate } from 'react-router-dom';
-import LoginModal from '../modal/loginModal'
+import LoginModal from '../modal/loginModal';
 
 function Header() {
-    const history = useNavigate()
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const [showLogin, setShowLogin] = useState(false);
-    const handleLoginClose = () => setShowLogin(false);
-    const handleLoginShow = () => setShowLogin(true);
+  const history = useNavigate();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [showLogin, setShowLogin] = useState(false);
+  const handleLoginClose = () => setShowLogin(false);
+  const handleLoginShow = () => setShowLogin(true);
+  const user = useSelector((state) => state.auth.user);
 
-    return (
-        <>
-            <Sidebar
-                show={show}
-                handleClose={handleClose}
-            />
-            <LoginModal handleClose={handleLoginClose} show={showLogin}/>
-            <div className="headerWrapper" id="header">
-                <Container fluid>
-                    <Row className="align-items-center">
-                        <Col lg={3} md={3} xs={3}>
-                            <div className="headerBarWrapper" onClick={handleShow}>
-                                <i className="fa-solid fa-bars headerBarsIcon"></i>
-                            </div>
-                        </Col>
-                        <Col lg={6} md={6} xs={6}>
-                            <div className="headerLogoWrapper"
-                                onClick={() => history.push('/')}
-                            >
-                                <p className="text-center">
-                                    <img src={logo} alt="" className="headerLogo" />
-                                </p>
-                            </div>
-                        </Col>
-                        <Col lg={3} md={3} xs={3}>
-                            <div className="pc "
-                                style={{ paddingRight: '10px' }} >
-                                <div className='d-flex justify-content-end align-items-center'>
-                                    <div className="headerButtonWrapper d-flex justify-content-center ">
-                                        <div className="headerBtn1">
-                                            <a className="text-center">Get Started</a>
-                                        </div>
-                                        <div onClick={handleLoginShow} className="headerBtn2">
-                                            <a className="text-center">Log In</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* <div className="res">
-                                <div className="headerContactWrapper">
-                                    <i className="fa-solid fa-user headerContactIcon"></i>
-                                </div>
-                            </div> */}
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Sidebar show={show} handleClose={handleClose} />
+      <LoginModal handleClose={handleLoginClose} show={showLogin} />
+      <div className="headerWrapper" id="header">
+        <Container fluid>
+          <Row className="align-items-center">
+            <Col lg={3} md={3} xs={3}>
+              <div className="headerBarWrapper" onClick={handleShow}>
+                <i className="fa-solid fa-bars headerBarsIcon"></i>
+              </div>
+            </Col>
+            <Col lg={6} md={6} xs={6}>
+              <div
+                className="headerLogoWrapper"
+                onClick={() => history.push('/')}
+              >
+                <p className="text-center">
+                  <img src={logo} alt="" className="headerLogo" />
+                </p>
+              </div>
+            </Col>
+            <Col lg={3} md={3} xs={3}>
+              <div className="pc " style={{ paddingRight: '10px' }}>
+                <div className="d-flex justify-content-end align-items-center">
+                  <div className="headerButtonWrapper d-flex justify-content-center ">
+                    <div className="headerBtn1">
+                      <a className="text-center">Get Started</a>
+                    </div>
+                    <div onClick={handleLoginShow} className="headerBtn2">
+                      <a className="text-center">Log In</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/modal/loginModalViews/recoverForm.jsx
+++ src/components/modal/loginModalViews/recoverForm.jsx
@@ -1,49 +1,59 @@
 import React, { useState } from 'react';
 import { resetPassword } from '../../../services/util';
+import '../modal.css';
 
 function RecoverForm(props) {
   const [email, setEmail] = useState({ value: '', touch: false, error: false });
 
-  const [message, setMessage] = useState({ message: '', error: false, general: false });
+  const [message, setMessage] = useState({
+    message: '',
+    error: false,
+    general: false,
+  });
   const [loading, setLoading] = useState(false);
 
   const submitHandler = async (e) => {
-      setLoading(true);
-      e.preventDefault();
-      let resetResponse = await resetPassword({ email: email.value.trim() });
-      if (resetResponse.data.code === 'ABT0000') {
-          setMessage({ message: resetResponse.data.message, error: false, general: true });
-          setEmail({ ...email, value: '' });
-          localStorage.setItem('isMovieChangePassword', true);
-      } else {
-          setMessage({ message: resetResponse.data.message, error: true, general: true });
-      }
-      setLoading(false);
+    setLoading(true);
+    e.preventDefault();
+    let resetResponse = await resetPassword({ email: email.value.trim() });
+    if (resetResponse.data.code === 'ABT0000') {
+      setMessage({
+        message: resetResponse.data.message,
+        error: false,
+        general: true,
+      });
+      setEmail({ ...email, value: '' });
+      localStorage.setItem('isMovieChangePassword', true);
+    } else {
+      setMessage({
+        message: resetResponse.data.message,
+        error: true,
+        general: true,
+      });
+    }
+    setLoading(false);
   };
-
 
   return (
     <>
-        <div>Forgot Password</div>
+      <div>Forgot Password</div>
       <form onSubmit={submitHandler}>
-        <div
-          style={{ display: 'flex', alignItems: 'center', marginTop: '16px' }}
-        >
+        <div className="form-container">
           <div style={{ flex: 1 }}>
             <label htmlFor="email">Email:</label>
             <input
-                                    required
-                                    type="email"
-                                    value={email.value}
-                                    onChange={(e) =>
-                                        setEmail({
-                                            value: e.target.value,
-                                            touch: true,
-                                            error: e.target.value ? false : true
-                                        })
-                                    }
-                                    className="form-control login-form__input"
-                                    placeholder="Email"
+              required
+              type="email"
+              value={email.value}
+              onChange={(e) =>
+                setEmail({
+                  value: e.target.value,
+                  touch: true,
+                  error: e.target.value ? false : true,
+                })
+              }
+              className="form-control login-form__input"
+              placeholder="Email"
             />
           </div>
         </div>
