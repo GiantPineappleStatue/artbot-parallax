5607c6903443354c3fae7ace5616dc56e3d95531
Jerry Wu <jerrywu@Jerrys-Mac-mini.local>
2023-02-15 13:18:39 -0800

    youtube fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
-import HomeSection from './home/home';
+import HomeSection from './home/video';
 import ctaImage from '../assets/Sparkle_Clapper.png';
 
 const Hero = (props) => {
   return (
     <section
-      className="section hero-content"
-      style={{ backgroundImage: `url(/images/${props.index - 1}.png)` }}
+      className="hero-section hero-content"
+      // style={{ backgroundImage: `url(/images/${props.index - 1}.png)` }}
       ref={props.registerSection(props.index)}
     >
       <HomeSection />
